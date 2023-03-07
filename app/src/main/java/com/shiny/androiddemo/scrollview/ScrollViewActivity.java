f13c9e1920f77298b392992e5c8b3e49ff7ac7ba
1S-CAOSN <1113068081@qq.com>
2023-03-07 16:20:47 +0800

    scrollview滚动视图 2023/3/7 14:51

---
+++ app/src/main/java/com/shiny/androiddemo/scrollview/ScrollViewActivity.java
@@ -0,0 +1,16 @@
+package com.shiny.androiddemo.scrollview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.shiny.androiddemo.R;
+
+public class ScrollViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_view);
+    }
+}
