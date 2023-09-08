0bb2f9e0b207279605cf2fd72d9d6e5800ad3956
1S-CAOSN <1113068081@qq.com>
2023-09-08 14:28:11 +0800

    videoview播放mp4 2023/4/19 10:21

--- app/src/main/java/com/shiny/androiddemo/utils/ConvertUtil.java
+++ app/src/main/java/com/shiny/androiddemo/utils/ConvertUtil.java
@@ -2,6 +2,10 @@ package com.shiny.androiddemo.utils;
 
 import android.content.Context;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class ConvertUtil {
     // 根据手机分辨率从 dp 转为 px
     public static int dpToPx(Context context, float dp) {
@@ -24,5 +28,12 @@ public class ConvertUtil {
     public static int px2sp(Context context,float pxValue){
         float fontScale=context.getResources().getDisplayMetrics().scaledDensity;
         return (int) (pxValue/fontScale+0.5f);
+    }
+
+    public static void soundByteArrToMp3(String mp3Path, byte[] soundByteArr) throws IOException {
+        File mp3File = new File(mp3Path);
+        FileOutputStream fos = new FileOutputStream(mp3File);
+        fos.write(soundByteArr);
+        fos.close();
     }
 }
--- app/src/main/java/com/shiny/androiddemo/videoview/VideoViewActivity.java
+++ app/src/main/java/com/shiny/androiddemo/videoview/VideoViewActivity.java
@@ -1,6 +1,10 @@
 package com.shiny.androiddemo.videoview;
 
 import android.content.Context;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -11,8 +15,12 @@ import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
 import com.shiny.androiddemo.R;
-import com.shiny.androiddemo.utils.Mp4ParserUtil;
+
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,6 +29,8 @@ public class VideoViewActivity extends AppCompatActivity {
     private static final String  TAG = "VideoViewActivity";
 
     private VideoView vv;
+    private MediaPlayer mp;
+    private AudioTrack at;
     private Button btn_player;
     private Button convert;
     private MediaController mc;
@@ -34,25 +44,79 @@ public class VideoViewActivity extends AppCompatActivity {
         btn_player = findViewById(R.id.btn_play);
         convert = findViewById(R.id.convert);
 
-        // Uri rtsp = Uri.parse("rtsp://192.168.201.1:554/live/stream0");
-        // vv.setVideoURI(rtsp);
+        String downloadDir = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).toString();
+        String h264Path = downloadDir + File.separatorChar + "2.h264";
+        String audioPath = downloadDir + File.separatorChar + "1.aud";
+        String mp4Path = downloadDir + File.separatorChar + "3.mp4";
+        String mp3Path = downloadDir + File.separatorChar + "temp.mp3";
+        String videoPath = downloadDir + File.separatorChar + "2.mp4";
 
-        String videoPath = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).toString() + File.separatorChar + "2.mp4";
+
         vv.setVideoPath(videoPath);
         vv.requestFocus();
 
+        at = new AudioTrack(AudioManager.STREAM_MUSIC, 24000, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, 24000, AudioTrack.MODE_STREAM);
+        byte[] sounds = null;
+        try {
+            sounds = FileUtils.readFileToByteArray(new File(audioPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        byte[] finalSounds = sounds;
         btn_player.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (!vv.isPlaying()) {
                     vv.start();
+                    at.play();
+                    at.write(finalSounds, 0 , finalSounds.length);
                     btn_player.setText("暂停");
                 } else {
                     vv.pause();
+                    at.pause();
                     btn_player.setText("播放");
                 }
             }
         });
+
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick: " + "准备开始转码!!!!");
+                try {
+                    // ConvertUtil.soundByteArrToMp3(downloadDir + File.separatorChar + "temp.mp3", FileUtils.readFileToByteArray(new File(audioPath)));
+                    String[] command = {"-y", "-i", h264Path, "-c:v", "copy", "-c:a", "copy", mp4Path};
+                    FFmpeg ffmpeg = FFmpeg.getInstance(context);
+                    ffmpeg.execute(command, new ExecuteBinaryResponseHandler() {
+                        @Override
+                        public void onSuccess(String message) {
+                            Log.d("FFmpeg", "Success: " + message);
+                        }
+                        @Override
+                        public void onFailure(String message) {
+                            Log.d("FFmpeg", "Failure: " + message);
+                        }
+                        @Override
+                        public void onProgress(String message) {
+                            Log.d("FFmpeg", "Progress: " + message);
+                        }
+                        @Override
+                        public void onStart() {
+                            Log.d("FFmpeg", "Started");
+                        }
+                        @Override
+                        public void onFinish() {
+                            Log.d("FFmpeg", "Finished");
+                        }
+                    });
+                } catch (FFmpegCommandAlreadyRunningException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
 
         /**
          * description: 进度条绑定到videoview控件上
@@ -63,19 +127,5 @@ public class VideoViewActivity extends AppCompatActivity {
             vv.setMediaController(mc);
             mc.setAnchorView(vv);
         }));
-
-        String h264Path = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).toString() + File.separatorChar + "2.h264";
-        String mp4Path = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).toString() + File.separatorChar + "3.mp4";
-        convert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "onClick: " + "准备开始转码!!!!");
-                try {
-                    Mp4ParserUtil.parser(h264Path, mp4Path);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
 }
